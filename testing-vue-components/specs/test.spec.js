de477c62f865eac77e40f012c2532c62d592dace
lucaspineda <lucas.pineda@hotmail.com>
2021-05-02 23:05:07 -0300

    mounting the vue component for testing

--- testing-vue-components/specs/test.spec.js
+++ testing-vue-components/specs/test.spec.js
@@ -1,6 +1,17 @@
 import TestComponent from '@/test'
-import {mount} from '@vue/test-utils'
+import List from '@/list'
+import { mount, shallowMount } from '@vue/test-utils'
 
-test('first-spec ', () => {
-  console.log(TestComponent)
+test('mount', () => {
+  const wrapper = mount(TestComponent, {
+    propsData: {
+      value: 'Another Test'
+    }
+  })
+  expect(wrapper.html()).toMatchSnapshot()
+});
+
+test('ListComponent shallow', () => {
+  console.log(mount(List).html())
+  console.log(shallowMount(List).html())
 });
