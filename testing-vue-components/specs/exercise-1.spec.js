4b1fc3cddc8bac168b949c6486f7dbda4c6d7514
lucaspineda <lucas.pineda@hotmail.com>
2021-05-06 00:39:23 -0300

    exercise 1 complete

--- testing-vue-components/specs/exercise-1.spec.js
+++ testing-vue-components/specs/exercise-1.spec.js
@@ -1,2 +1,20 @@
 import UserList from '@/exercise-1';
 import { mount } from '@vue/test-utils';
+import { name } from 'faker'
+
+test('component renders the users', async () => {
+  const wrapper = mount(UserList, {
+    propsData: {
+      users: [name.findName(), name.findName(), name.findName()]
+    }
+  })
+  const li = wrapper.find('li')
+  const input = wrapper.find('input')
+  
+  expect(li.text()).toBe(wrapper.props('users')[0])
+
+  const namePicked = wrapper.props('users')[2]
+
+  await input.setValue(namePicked)
+  expect(wrapper.find('li').text()).toBe(namePicked)
+});
